7024db8f4e97bccabd362d89d9f4513921570c95
Doni Ramos da Silva <doni.silva@goias.gov.br>
2025-12-09 15:18:03 -0300

    test: ensure login shows error if email is invalid

--- src/components/pages/login/login.page.test.tsx
+++ src/components/pages/login/login.page.test.tsx
@@ -1,4 +1,5 @@
 import userEvent from '@testing-library/user-event'
+
 import { renderWithRedux } from '../../helpers/test.helpers'
 import LoginPage from './login.page'
 import { screen } from '@testing-library/react'
@@ -15,5 +16,21 @@ describe('Login', () => {
 
     await screen.findByText(/email is required/i)
     screen.getByText(/password is required/i)
+  })
+
+  it('should show error if email is invalid', async () => {
+    renderWithRedux(
+      <LoginPage />,
+      {}
+    )
+
+    const emailInput = screen.getByPlaceholderText(/enter your email/i)
+
+    userEvent.type(emailInput, 'invalid_email')
+
+    const submitButton = screen.getByText('Sign In')
+    userEvent.click(submitButton)
+
+    await screen.findByText(/invalid email/i)
   })
 })
