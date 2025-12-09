0aefc00cd1aa36775d6b8b7258738daf32eec6e4
Doni Ramos da Silva <doni.silva@goias.gov.br>
2025-12-09 15:49:51 -0300

    test: ensure sign up shows error if password has less than 6 characters.

--- src/components/pages/sign-up/sign-up.test.tsx
+++ src/components/pages/sign-up/sign-up.test.tsx
@@ -1,4 +1,5 @@
 import userEvent from '@testing-library/user-event'
+
 import { renderWithRedux } from '../../helpers/test.helpers'
 import SignUpPage from './sign-up.page'
 import { screen } from '@testing-library/react'
@@ -53,5 +54,21 @@ describe('Sign Up', () => {
     userEvent.click(submitButton)
 
     await screen.findByText(/passwords do not match/i)
+  })
+
+  it('should show error when password has less than 6 characters', async () => {
+    renderWithRedux(
+      <SignUpPage />,
+      {}
+    )
+
+    const passwordInput = screen.getByPlaceholderText(/enter your password/i)
+
+    userEvent.type(passwordInput, '123')
+
+    const submitButton = screen.getByText('Create Account', { selector: 'button' })
+    userEvent.click(submitButton)
+
+    await screen.findByText(/password must be at least 6 characters/i)
   })
 })
