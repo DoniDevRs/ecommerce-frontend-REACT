947ca9e043d878b26fc5f93c6e3a48086da58aa5
Doni Ramos da Silva <doni.silva@goias.gov.br>
2025-12-09 15:24:45 -0300

    test: ensure login shows error if email or password is invalid.

--- src/components/pages/login/login.page.test.tsx
+++ src/components/pages/login/login.page.test.tsx
@@ -1,4 +1,6 @@
 import userEvent from '@testing-library/user-event'
+import * as firebaseAuth from 'firebase/auth'
+import { AuthErrorCodes } from 'firebase/auth'
 
 import { renderWithRedux } from '../../helpers/test.helpers'
 import LoginPage from './login.page'
@@ -33,4 +35,56 @@ describe('Login', () => {
 
     await screen.findByText(/invalid email/i)
   })
+
+  it('should show an error if email is not found', async () => {
+    const mockFirebaseAuth = firebaseAuth as any
+
+    mockFirebaseAuth.signInWithEmailAndPassword.mockImplementation(() =>
+      Promise.reject({ code: AuthErrorCodes.USER_DELETED })
+    )
+
+    renderWithRedux(
+      <LoginPage />,
+      {}
+    )
+
+    const emailInput = screen.getByPlaceholderText(/enter your email/i)
+
+    userEvent.type(emailInput, 'lorem@ipsum.com')
+
+    const passwordInput = screen.getByPlaceholderText(/enter your password/i)
+    userEvent.type(passwordInput, '12345678')
+
+    const submitButton = screen.getByText('Sign In')
+
+    userEvent.click(submitButton)
+
+    await screen.findByText(/email not found/i)
+  })
+
+  it('should show an error if password is not valid', async () => {
+    const mockFirebaseAuth = firebaseAuth as any
+
+    mockFirebaseAuth.signInWithEmailAndPassword.mockImplementation(() =>
+      Promise.reject({ code: AuthErrorCodes.INVALID_PASSWORD })
+    )
+
+    renderWithRedux(
+      <LoginPage />,
+      {}
+    )
+
+    const emailInput = screen.getByPlaceholderText(/enter your email/i)
+
+    userEvent.type(emailInput, 'lorem@ipsum.com')
+
+    const passwordInput = screen.getByPlaceholderText(/enter your password/i)
+    userEvent.type(passwordInput, '123456')
+
+    const submitButton = screen.getByText('Sign In')
+
+    userEvent.click(submitButton)
+
+    await screen.findByText(/password is invalid/i)
+    })
 })
